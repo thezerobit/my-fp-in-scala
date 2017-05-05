e69701d301eb12c40b8af20fa5bb9f19a7bad9a1
Stephen A. Goss <steveth45@gmail.com>
2017-05-04 21:09:03 -0700

    reorganize List code, added exercises through 3.28

--- src/main/scala/exercises/Chapter3.scala
+++ src/main/scala/exercises/Chapter3.scala
@@ -1,159 +1,265 @@
 package exercises
 
-import book_code.list._
-import book_code.list.List._
-
 object Chapter3 {
-  // 3.1
-  val answer_3_1 = 3
 
-  // 3.2
-  def tail[A](list: List[A]): List[A] = list match {
-    case Cons(_, rest) => rest
-    case Nil => Nil
-  }
+  sealed trait List[+A]
 
-  // 3.3
-  def setHead[A](list: List[A], elem: A): List[A] = list match {
-    case Cons(_, rest) => Cons(elem, rest)
-    case Nil => List(elem)
-  }
+  // `List` data type, parameterized on a type, `A`
+  case object Nil extends List[Nothing]
 
-  // 3.4
-  @annotation.tailrec
-  def drop[A](list: List[A], n: Int): List[A] = {
-    if (n <= 0) list
-    else list match {
-      case Cons(_, rest) => drop(rest, n - 1)
+  // A `List` data constructor representing the empty list
+  /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
+  which may be `Nil` or another `Cons`.
+   */
+  case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+  object List {
+    // `List` companion object. Contains functions for creating and working with lists.
+    def sum(ints: List[Int]): Int = ints match {
+      // A function that uses pattern matching to add up a list of integers
+      case Nil => 0 // The sum of the empty list is 0.
+      case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    }
+
+    def product(ds: List[Double]): Double = ds match {
+      case Nil => 1.0
+      case Cons(0.0, _) => 0.0
+      case Cons(x, xs) => x * product(xs)
+    }
+
+    def apply[A](as: A*): List[A] = // Variadic function syntax
+      if (as.isEmpty) Nil
+      else Cons(as.head, apply(as.tail: _*))
+
+    val _3_1 = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + sum(t)
+      case _ => 101
+    }
+
+    def append[A](a1: List[A], a2: List[A]): List[A] =
+      a1 match {
+        case Nil => a2
+        case Cons(h, t) => Cons(h, append(t, a2))
+      }
+
+    def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+      as match {
+        case Nil => z
+        case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+      }
+
+    def sum2(ns: List[Int]) =
+      foldRight(ns, 0)((x, y) => x + y)
+
+    def product2(ns: List[Double]) =
+      foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
+
+    // 3.1
+    val answer_3_1 = 3
+
+    // 3.2
+    def tail[A](list: List[A]): List[A] = list match {
+      case Cons(_, rest) => rest
       case Nil => Nil
     }
-  }
 
-  // 3.5
-  @annotation.tailrec
-  def dropWhile[A](list: List[A], f: A => Boolean): List[A] = list match {
-    case Cons(first, rest) => if (f(first)) dropWhile(rest, f) else list
-    case Nil => Nil
-  }
-
-  // 3.6
-  def init[A](list: List[A]): List[A] = {
-    @annotation.tailrec
-    def reverse(l: List[A], accum: List[A]): List[A] = l match {
-      case Cons(first, rest) => reverse(rest, Cons(first, accum))
-      case Nil => accum
+    // 3.3
+    def setHead[A](list: List[A], elem: A): List[A] = list match {
+      case Cons(_, rest) => Cons(elem, rest)
+      case Nil => List(elem)
     }
-    val reversed = reverse(list, Nil)
-    reversed match {
-      case Cons(_, rest) => reverse(rest, Nil)
+
+    // 3.4
+    @annotation.tailrec
+    def drop[A](list: List[A], n: Int): List[A] = {
+      if (n <= 0) list
+      else list match {
+        case Cons(_, rest) => drop(rest, n - 1)
+        case Nil => Nil
+      }
+    }
+
+    // 3.5
+    @annotation.tailrec
+    def dropWhile[A](list: List[A], f: A => Boolean): List[A] = list match {
+      case Cons(first, rest) => if (f(first)) dropWhile(rest, f) else list
       case Nil => Nil
     }
-  }
 
-  // 3.7
-  // no, foldRight itself has no mechanism for short-circuiting
+    // 3.6
+    def init[A](list: List[A]): List[A] = {
+      @annotation.tailrec
+      def reverse(l: List[A], accum: List[A]): List[A] = l match {
+        case Cons(first, rest) => reverse(rest, Cons(first, accum))
+        case Nil => accum
+      }
 
-  // 3.8
-  def seeWhatHappens(): Unit = {
-    println(foldRight(List(1,2,3), Nil: List[Int])(Cons(_,_)))
-  }
-  // The application of the provided function in foldRight correlates to
-  // each Cons construction
+      val reversed = reverse(list, Nil)
+      reversed match {
+        case Cons(_, rest) => reverse(rest, Nil)
+        case Nil => Nil
+      }
+    }
 
-  // 3.9
-  def length[A](list: List[A]): Int = foldRight(list, 0)((_,y) => 1 + y)
+    // 3.7
+    // no, foldRight itself has no mechanism for short-circuiting
 
-  // 3.10
-  @annotation.tailrec
-  def foldLeft[A,B](list: List[A], initial: B)(f: (B, A) => B): B = list match {
-    case Cons(first, rest) => foldLeft(rest,f(initial, first))(f)
-    case Nil => initial
-  }
+    // 3.8
+    def seeWhatHappens(): Unit = {
+      println(foldRight(List(1, 2, 3), Nil: List[Int])(Cons(_, _)))
+    }
 
-  // 3.11
-  def mySum(list: List[Int]): Int = foldLeft(list, 0)(_ + _)
+    // The application of the provided function in foldRight correlates to
+    // each Cons construction
 
-  def myProduct(list: List[Int]): Int = foldLeft(list, 1)(_ * _)
+    // 3.9
+    def length[A](list: List[A]): Int = foldRight(list, 0)((_, y) => 1 + y)
 
-  def myLength(list: List[Int]): Int = foldLeft(list, 0)((count, _) => count + 1)
-
-  // 3.12
-  def reverse[A](list: List[A]): List[A] = {
+    // 3.10
     @annotation.tailrec
-    def _reverse(l: List[A], accum: List[A]): List[A] = l match {
-      case Cons(first, rest) => _reverse(rest, Cons(first, accum))
-      case Nil => accum
+    def foldLeft[A, B](list: List[A], initial: B)(f: (B, A) => B): B = list match {
+      case Cons(first, rest) => foldLeft(rest, f(initial, first))(f)
+      case Nil => initial
     }
-    _reverse(list, Nil)
-  }
 
-  def reverse2[A](list: List[A]): List[A] = {
-    foldLeft[A,List[A]](list, Nil)((b, a) => Cons(a, b))
-  }
+    // 3.11
+    def mySum(list: List[Int]): Int = foldLeft(list, 0)(_ + _)
 
-  // 3.13
-  def foldLeft2[A,B](list: List[A], initial: B)(f: (B, A) => B): B = {
-    foldRight(reverse(list), initial)((a, b) => f(b, a))
-  }
+    def myProduct(list: List[Int]): Int = foldLeft(list, 1)(_ * _)
 
-  def foldRight2[A,B](list: List[A], initial: B)(f: (A, B) => B): B = {
-    foldLeft(reverse(list), initial)((b, a) => f(a, b))
-  }
+    def myLength(list: List[Int]): Int = foldLeft(list, 0)((count, _) => count + 1)
 
-  // 3.14
-  def append2[A](l1: List[A], l2: List[A]): List[A] = {
-    foldLeft(reverse(l1), l2)((b, a) => Cons(a, b))
-  }
+    // 3.12
+    def reverse[A](list: List[A]): List[A] = {
+      @annotation.tailrec
+      def _reverse(l: List[A], accum: List[A]): List[A] = l match {
+        case Cons(first, rest) => _reverse(rest, Cons(first, accum))
+        case Nil => accum
+      }
 
-  // 3.15
-  def concatenate[A](list: List[List[A]]): List[A] = {
-    foldRight2[List[A], List[A]](list, Nil)((a, b) => append2(a, b))
-  }
+      _reverse(list, Nil)
+    }
 
-  // 3.16
-  def addOneToEach(list: List[Int]): List[Int] = {
-    foldRight2[Int,List[Int]](list, Nil)((a, b) => Cons(a + 1, b))
-  }
+    def reverse2[A](list: List[A]): List[A] = {
+      foldLeft[A, List[A]](list, Nil)((b, a) => Cons(a, b))
+    }
 
-  // 3.17
-  def flarp(list: List[Double]): List[String] = {
-    foldRight2[Double, List[String]](list, Nil)((a, b) => Cons(a.toString, b))
-  }
+    // 3.13
+    def foldLeft2[A, B](list: List[A], initial: B)(f: (B, A) => B): B = {
+      foldRight(reverse(list), initial)((a, b) => f(b, a))
+    }
 
-  // 3.18
-  def map[A,B](list: List[A])(f: A => B): List[B] = {
-    foldRight2[A, List[B]](list, Nil)((a, b) => Cons(f(a), b))
-  }
+    def foldRight2[A, B](list: List[A], initial: B)(f: (A, B) => B): B = {
+      foldLeft(reverse(list), initial)((b, a) => f(a, b))
+    }
 
-  // 3.19
-  def filter[A](as: List[A])(f: A => Boolean): List[A] = {
-    foldRight2[A, List[A]](reverse(as), Nil)((a, b) => if (f(a)) Cons(a, b) else b)
-  }
+    // 3.14
+    def append2[A](l1: List[A], l2: List[A]): List[A] = {
+      foldLeft(reverse(l1), l2)((b, a) => Cons(a, b))
+    }
 
-  // 3.20
-  def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = {
-    foldRight2[A, List[B]](as, Nil)((a, b) => append2(f(a), b))
-  }
+    // 3.15
+    def concatenate[A](list: List[List[A]]): List[A] = {
+      foldRight2[List[A], List[A]](list, Nil)((a, b) => append2(a, b))
+    }
 
-  // 3.21
-  def filter2[A](as: List[A])(f: A => Boolean): List[A] = {
-    flatMap(as)(x => if (f(x)) List(x) else Nil)
-  }
+    // 3.16
+    def addOneToEach(list: List[Int]): List[Int] = {
+      foldRight2[Int, List[Int]](list, Nil)((a, b) => Cons(a + 1, b))
+    }
 
-  // 3.22
-  def addElems(l1: List[Int], l2: List[Int]): List[Int] = {
-    (l1, l2) match {
-      case (Cons(a, as), Cons(b, bs)) => Cons(a + b, addElems(as, bs))
-      case _ => Nil
+    // 3.17
+    def flarp(list: List[Double]): List[String] = {
+      foldRight2[Double, List[String]](list, Nil)((a, b) => Cons(a.toString, b))
+    }
+
+    // 3.18
+    def map[A, B](list: List[A])(f: A => B): List[B] = {
+      foldRight2[A, List[B]](list, Nil)((a, b) => Cons(f(a), b))
+    }
+
+    // 3.19
+    def filter[A](as: List[A])(f: A => Boolean): List[A] = {
+      foldRight2[A, List[A]](reverse(as), Nil)((a, b) => if (f(a)) Cons(a, b) else b)
+    }
+
+    // 3.20
+    def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = {
+      foldRight2[A, List[B]](as, Nil)((a, b) => append2(f(a), b))
+    }
+
+    // 3.21
+    def filter2[A](as: List[A])(f: A => Boolean): List[A] = {
+      flatMap(as)(x => if (f(x)) List(x) else Nil)
+    }
+
+    // 3.22
+    def addElems(l1: List[Int], l2: List[Int]): List[Int] = {
+      (l1, l2) match {
+        case (Cons(a, as), Cons(b, bs)) => Cons(a + b, addElems(as, bs))
+        case _ => Nil
+      }
+    }
+
+    // 3.23
+    def zipWith[A, B, C](l1: List[A], l2: List[B])(f: (A, B) => C): List[C] = {
+      (l1, l2) match {
+        case (Cons(a, as), Cons(b, bs)) => Cons(f(a, b), zipWith(as, bs)(f))
+        case _ => Nil
+      }
+    }
+
+    // 3.24
+    @annotation.tailrec
+    def startsWith[A](list: List[A], sub: List[A]): Boolean = {
+      (list, sub) match {
+        case (Cons(a, as), Cons(b, bs)) if a == b => startsWith(as, bs)
+        case (_, Nil) => true
+        case _ => false
+      }
+    }
+
+    @annotation.tailrec
+    def hasSubsequence[A](list: List[A], sub: List[A]): Boolean = {
+      list match {
+        case Cons(_, as) => if (startsWith(list, sub)) true else hasSubsequence(as, sub)
+        case Nil => sub == Nil
+      }
     }
   }
 
-  // 3.23
-  def zipWith[A,B,C](l1: List[A], l2: List[B])(f: (A, B) => C): List[C] = {
-    (l1, l2) match {
-      case (Cons(a, as), Cons(b, bs)) => Cons(f(a, b), zipWith(as, bs)(f))
-      case _ => Nil
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  object Tree {
+    // 3.25
+    def size[A](tree: Tree[A]): Int = tree match {
+      case Branch(l, t) => 1 + size(l) + size(t)
+      case Leaf(_) => 1
     }
+
+    // 3.26
+    def max(tree: Tree[Int]): Int = tree match {
+      case Leaf(x) => x
+      case Branch(l, r) => max(l) max max(r)
+    }
+
+    // 3.27
+    def depth[A](tree: Tree[A]): Int = tree match {
+      case Leaf(_) => 1
+      case Branch(l, r) => 1 + (depth(l) max depth(r))
+    }
+
+    // 3.28
+    def map[A,B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+      case Leaf(x) => Leaf(f(x))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    }
+
   }
 
 }
--- src/test/scala/exercises/Chapter3Spec.scala
+++ src/test/scala/exercises/Chapter3Spec.scala
@@ -1,117 +1,147 @@
 package exercises
 
-import book_code.list._
 import exercises.Chapter3._
 import org.scalatest.FlatSpec
 
 class Chapter3Spec extends FlatSpec {
-  "answer_3_1" should "match the correct answer" in {
-    assert(answer_3_1 == List.x)
+
+  "List.answer_3_1" should "match the correct answer" in {
+    assert(List.answer_3_1 == List._3_1)
   }
 
-  "tail" should "return the tail of the List, if there is one" in {
-    assert(tail(List(1,2,3)) == List(2,3))
-    assert(tail(Nil) == Nil)
+  "List.tail" should "return the tail of the List, if there is one" in {
+    assert(List.tail(List(1,2,3)) == List(2,3))
+    assert(List.tail(Nil) == Nil)
   }
 
-  "setHead" should "replace the first element in a List or add if list is empty" in {
-    assert(setHead(List(1,2,3), 0) == List(0,2,3))
-    assert(setHead(Nil, 0) == List(0))
+  "List.setHead" should "replace the first element in a List or add if list is empty" in {
+    assert(List.setHead(List(1,2,3), 0) == List(0,2,3))
+    assert(List.setHead(Nil, 0) == List(0))
   }
 
-  "drop" should "drop as many elements as specified or until reaching Nil" in {
-    assert(drop(List(1,2,3), 1) == List(2,3))
-    assert(drop(List(1,2,3), 2) == List(3))
-    assert(drop(List(1,2,3), 3) == Nil)
-    assert(drop(List(1,2,3), 4) == Nil)
+  "List.drop" should "drop as many elements as specified or until reaching Nil" in {
+    assert(List.drop(List(1,2,3), 1) == List(2,3))
+    assert(List.drop(List(1,2,3), 2) == List(3))
+    assert(List.drop(List(1,2,3), 3) == Nil)
+    assert(List.drop(List(1,2,3), 4) == Nil)
   }
 
-  "dropWhile" should "drop it while its hot" in {
-    assert(dropWhile(List(1,2,3), (x: Int) => x < 3) == List(3))
-    assert(dropWhile(List(1,2,3), (x: Int) => x > 3) == List(1,2,3))
-    assert(dropWhile(List(1,2,3), (x: Int) => x < 4) == Nil)
+  "List.dropWhile" should "drop it while its hot" in {
+    assert(List.dropWhile(List(1,2,3), (x: Int) => x < 3) == List(3))
+    assert(List.dropWhile(List(1,2,3), (x: Int) => x > 3) == List(1,2,3))
+    assert(List.dropWhile(List(1,2,3), (x: Int) => x < 4) == Nil)
   }
 
-  "init" should "drop the last element from a list, if there is one" in {
-    assert(init(List(1,2,3,4,5,6,7,8)) == List(1,2,3,4,5,6,7))
-    assert(init(Nil) == Nil)
+  "List.init" should "drop the last element from a list, if there is one" in {
+    assert(List.init(List(1,2,3,4,5,6,7,8)) == List(1,2,3,4,5,6,7))
+    assert(List.init(Nil) == Nil)
   }
 
-  "length" should "give the length of a list" in {
-    assert(length(List(1,2,3)) == 3)
-    assert(length(Nil) == 0)
+  "List.length" should "give the length of a list" in {
+    assert(List.length(List(1,2,3)) == 3)
+    assert(List.length(Nil) == 0)
   }
 
-  "foldLeft" should "fold like a champ" in {
-    assert(foldLeft(List(1,2,3), 0)(_ + _) == 6)
-    assert(foldLeft(List("a","b","c"), "")(_ + _) == "abc")
+  "List.foldLeft" should "fold like a champ" in {
+    assert(List.foldLeft(List(1,2,3), 0)(_ + _) == 6)
+    assert(List.foldLeft(List("a","b","c"), "")(_ + _) == "abc")
   }
 
-  "mySum" should "sum a list of integers" in {
-    assert(mySum(List(1,2,3,4)) == 10)
-    assert(mySum(Nil) == 0)
+  "List.mySum" should "sum a list of integers" in {
+    assert(List.mySum(List(1,2,3,4)) == 10)
+    assert(List.mySum(Nil) == 0)
   }
 
-  "myProduct" should "calculate the product of a list of integers" in {
-    assert(myProduct(List(1,2,3)) == 6)
-    assert(myProduct(Nil) == 1)
+  "List.myProduct" should "calculate the product of a list of integers" in {
+    assert(List.myProduct(List(1,2,3)) == 6)
+    assert(List.myProduct(Nil) == 1)
   }
 
-  "myLength" should "calculate the length of a list" in {
-    assert(myLength(List(1,2,3)) == 3)
-    assert(myLength(Nil) == 0)
+  "List.myLength" should "calculate the length of a list" in {
+    assert(List.myLength(List(1,2,3)) == 3)
+    assert(List.myLength(Nil) == 0)
   }
 
-  "reverse2" should "reverse a list" in {
-    assert(reverse(List(1,2,3)) == List(3,2,1))
+  "List.reverse" should "reverse a list" in {
+    assert(List.reverse(List(1,2,3)) == List(3,2,1))
   }
 
-  "foldLeft2" should "calculate foldLeft in terms of foldRight" in {
-    assert(foldLeft2(List("a","b","c"), "")(_ + _) == "abc")
+  "List.foldLeft2" should "calculate foldLeft in terms of foldRight" in {
+    assert(List.foldLeft2(List("a","b","c"), "")(_ + _) == "abc")
   }
 
-  "foldRight2" should "calculate foldRight in terms of foldLeft" in {
-    assert(foldRight2(List("a","b","c"), "")(_ + _) == "abc")
+  "List.foldRight2" should "calculate foldRight in terms of foldLeft" in {
+    assert(List.foldRight2(List("a","b","c"), "")(_ + _) == "abc")
   }
 
-  "append2" should "append 2 lists" in {
-    assert(append2(List(1,2,3), List(4,5,6)) == List(1,2,3,4,5,6))
+  "List.append2" should "append 2 lists" in {
+    assert(List.append2(List(1,2,3), List(4,5,6)) == List(1,2,3,4,5,6))
   }
 
-  "concatenate" should "append a list of lists into one list" in {
-    assert(concatenate(List(List(1,2,3), List(4,5,6), List(7,8,9))) == List(1,2,3,4,5,6,7,8,9))
+  "List.concatenate" should "append a list of lists into one list" in {
+    assert(List.concatenate(List(List(1,2,3), List(4,5,6), List(7,8,9))) == List(1,2,3,4,5,6,7,8,9))
   }
 
-  "addOneToEach" should "add 1 to each element of a list" in {
-    assert(addOneToEach(List(1,2,3)) == List(2,3,4))
+  "List.addOneToEach" should "add 1 to each element of a list" in {
+    assert(List.addOneToEach(List(1,2,3)) == List(2,3,4))
   }
 
-  "flarp" should "convert a list of Double to a list of String" in {
-    assert(flarp(List(1.0, 2.2)) == List("1.0", "2.2"))
+  "List.flarp" should "convert a list of Double to a list of String" in {
+    assert(List.flarp(List(1.0, 2.2)) == List("1.0", "2.2"))
   }
 
-  "map" should "map" in {
-    assert(map(List(1,2,3))(x => x * 2) == List(2,4,6))
+  "List.map" should "map" in {
+    assert(List.map(List(1,2,3))(x => x * 2) == List(2,4,6))
   }
 
-  "filter" should "return a list with items that satisfy predicate" in {
-    assert(filter(List(1,2,3))(x => x % 2 == 0) == List(2))
+  "List.filter" should "return a list with items that satisfy predicate" in {
+    assert(List.filter(List(1,2,3))(x => x % 2 == 0) == List(2))
   }
 
-  "flatMap" should "map, flatly" in {
-    assert(flatMap(List(1,2,3))(x => List(x, -x)) == List(1,-1,2,-2,3,-3))
+  "List.flatMap" should "map, flatly" in {
+    assert(List.flatMap(List(1,2,3))(x => List(x, -x)) == List(1,-1,2,-2,3,-3))
   }
 
-  "filter2" should "return a list with items that satisfy predicate" in {
-    assert(filter2(List(1,2,3))(x => x % 2 == 0) == List(2))
+  "List.filter2" should "return a list with items that satisfy predicate" in {
+    assert(List.filter2(List(1,2,3))(x => x % 2 == 0) == List(2))
   }
 
-  "addElems" should "combine lists of numbers elementwise with addition" in {
-    assert(addElems(List(1,2,3), List(10, 20, 30)) == List(11, 22, 33))
+  "List.addElems" should "combine lists of numbers elementwise with addition" in {
+    assert(List.addElems(List(1,2,3), List(10, 20, 30)) == List(11, 22, 33))
   }
 
-  "zipWith" should "combine lists with an arbitrary binary function" in {
-    assert(zipWith(List(1,2,3), List(10,20,30))(_ * _) == List(10, 40, 90))
+  "List.zipWith" should "combine lists with an arbitrary binary function" in {
+    assert(List.zipWith(List(1,2,3), List(10,20,30))(_ * _) == List(10, 40, 90))
   }
+
+  "List.hasSubsequence" should "detect if a list contains another as a subsequence" in {
+    assert(List.hasSubsequence(Nil, Nil))
+    assert(List.hasSubsequence(List(1,2,3), Nil))
+    assert(List.hasSubsequence(List(1,2,3,4,5), List(1)))
+    assert(List.hasSubsequence(List(1,2,3,4,5), List(1,2,3)))
+    assert(List.hasSubsequence(List(1,2,3,4,5), List(2,3,4)))
+    assert(List.hasSubsequence(List(1,2,3,4,5), List(4,5)))
+    assert(List.hasSubsequence(List(1,2,3,4,5), List(4)))
+    assert(List.hasSubsequence(List(1,2,3,4,5), List(5)))
+    assert(!List.hasSubsequence(List(1,2,3), List(3,2)))
+    assert(!List.hasSubsequence(List(1,2,3), List(4)))
+    assert(!List.hasSubsequence(List(1,2,3), List(1,2,3,4)))
+  }
+
+  "Tree.size" should "count the leaves and branches" in {
+    assert(Tree.size(Branch(Leaf(1), Leaf(2))) == 3)
+    assert(Tree.size(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) == 5)
+  }
+
+  "Tree.max" should "find the largest value in tree" in {
+    assert(Tree.max(Branch(Leaf(1), Leaf(2))) == 2)
+    assert(Tree.max(Branch(Branch(Leaf(1), Leaf(3)), Leaf(2))) == 3)
+  }
+
+  "Tree.depth" should "find the maximum path length from root to leaf" in {
+    assert(Tree.depth(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) == 3)
+  }
+
+
 
 }
